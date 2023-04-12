23d09d0f0d437d76ef9f4c8a9027d4f9ef5b49e6
Wiliam Traoré <hi@wil-liam.com>
2023-04-12 20:46:45 +0200

    chore(project): add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="App bg-red-500">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
