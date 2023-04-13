7fdf80772ac1026724b86cb5e66284a757a35856
Wiliam Traoré <hi@wil-liam.com>
2023-04-13 22:43:16 +0200

    chore(project): add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 export default function App() {
-  return <p>Streali</p>
+  return <p>Streali</p>;
 }
