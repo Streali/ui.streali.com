219edc83c89427576270707bf6fe56cb4db32133
Wiliam Traoré <hi@wil-liam.com>
2023-05-04 15:23:54 +0200

    fix(radio): change text to label

--- src/components/forms/radio/radio.tsx
+++ src/components/forms/radio/radio.tsx
@@ -20,16 +20,17 @@ function Radio(props: RadioProps) {
     <RadioGroup.Root
       name={name}
       defaultValue={defaultValue}
-      className={`flex gap-3 ${align === 'vertical' ? 'flex-col' : 'flex-row'}`}>
+      className={`flex gap-4 ${align === 'vertical' ? 'flex-col' : 'flex-row'}`}>
       {items.map((item) => (
         <div className="flex items-center gap-2" key={item.value}>
           <RadioGroup.Item
+            id={item.value}
             key={item.value}
             value={item.value}
-            className="w-5 h-5 bg-grey-700 border border-grey-400 rounded-full data-[state=checked]:bg-primary-500 transition-colors duration-200">
+            className="w-5 h-5 bg-grey-700 border border-grey-300 rounded-full data-[state=checked]:bg-primary-500 transition-colors duration-200">
             <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-2 after:h-2 after:rounded-[50%] after:bg-white" />
           </RadioGroup.Item>
-          <Text>{item.label}</Text>
+          <label htmlFor={item.value}>{item.label}</label>
         </div>
       ))}
     </RadioGroup.Root>
