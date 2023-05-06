f576bad1938e3223cabe6a4467729949c3ffa94e
Wiliam Traoré <hi@wil-liam.com>
2023-05-06 14:21:33 +0200

    feat(navigation): add custom content

--- src/components/layout/shell/shell.tsx
+++ src/components/layout/shell/shell.tsx
@@ -2,30 +2,27 @@ import { PopoverMenuItem } from '../../popover-menu/popover-menu';
 import { Navigation, NavigationBottomItem, NavigationItem } from '../navigation/navigation';
 
 interface ShellProps {
-  logo: React.ReactNode;
-  user: {
-    username: string;
-    avatarUrl?: string;
-    menu: PopoverMenuItem[];
+  navigation: {
+    logo: React.ReactNode;
+    user: {
+      username: string;
+      avatarUrl?: string;
+      menu: PopoverMenuItem[];
+    };
+    navigation: NavigationItem[];
+    bottomNavigation: NavigationBottomItem[];
+    version: string;
+    customContent?: React.ReactNode;
   };
-  navigation: NavigationItem[];
-  bottomNavigation: NavigationBottomItem[];
-  version: string;
   children: React.ReactNode;
 }
 
 export function Shell(props: ShellProps) {
-  const { logo, user, navigation, bottomNavigation, version, children } = props;
+  const { navigation, children } = props;
 
   return (
     <>
-      <Navigation
-        logo={logo}
-        user={user}
-        navigation={navigation}
-        bottomNavigation={bottomNavigation}
-        version={version}
-      />
+      <Navigation {...navigation} />
       <div className="ml-[14rem] min-h-screen w-[calc(100vw_-_14rem)]">{children}</div>
     </>
   );
