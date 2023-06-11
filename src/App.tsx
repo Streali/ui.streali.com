944c837a4ee68dd9425eea21bdda4bc91f1cd205
Wiliam Traoré <hi@wil-liam.com>
2023-06-11 12:06:45 +0200

    fix(spacing): remove border

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-export default function App() {
-  return <p>Streali</p>;
+export function App() {
+  return <div>Streali UI</div>;
 }
--- src/components/editor/spacing/spacing.tsx
+++ src/components/editor/spacing/spacing.tsx
@@ -8,23 +8,21 @@ export type SpacingType = {
 };
 
 interface SpacingProps {
-  border: SpacingType;
   margin: SpacingType;
   padding: SpacingType;
-  onChange?: (value: { border: SpacingType; margin: SpacingType; padding: SpacingType }) => void;
+  onChange?: (value: { margin: SpacingType; padding: SpacingType }) => void;
 }
 
 export function Spacing(props: SpacingProps) {
-  const { border, margin, padding, onChange } = props;
+  const { margin, padding, onChange } = props;
 
   const handleValueChange = (
-    type: 'border' | 'margin' | 'padding',
+    type: 'margin' | 'padding',
     side: 'top' | 'right' | 'bottom' | 'left',
     value: number
   ) => {
     if (onChange) {
       onChange({
-        border: { ...border },
         margin: { ...margin },
         padding: { ...padding },
         [type]: { ...[type], [side]: value },
@@ -41,102 +39,67 @@ export function Spacing(props: SpacingProps) {
       <input
         type="number"
         min="0"
-        className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute top-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
+        className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute top-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
         defaultValue={margin.top}
         onChange={(e) => handleValueChange('margin', 'top', e.target.valueAsNumber)}
       />
       <input
         type="number"
         min="0"
-        className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
+        className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
         defaultValue={margin.bottom}
         onChange={(e) => handleValueChange('margin', 'bottom', e.target.valueAsNumber)}
       />
       <input
         type="number"
         min="0"
-        className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 left-0 -translate-y-1/2 text-sm align-middle appearance-none"
+        className="w-6 h-6 text-center bg-grey-900 border-none outline-none absolute top-1/2 left-0 -translate-y-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
         defaultValue={margin.left}
         onChange={(e) => handleValueChange('margin', 'left', e.target.valueAsNumber)}
       />
       <input
         type="number"
         min="0"
-        className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 right-0 -translate-y-1/2 text-sm align-middle appearance-none"
+        className="w-6 h-6 text-center bg-grey-900 border-none outline-none absolute top-1/2 right-0 -translate-y-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
         defaultValue={margin.right}
         onChange={(e) => handleValueChange('margin', 'right', e.target.valueAsNumber)}
       />
 
-      <div className="w-full bg-grey-800 border border-grey-400 rounded p-6 relative">
-        {/* Border */}
+      <div className="w-full bg-grey-700 border border-grey-400 rounded p-6 relative">
+        {/* Padding */}
         <Text type="medium" className="absolute top-1 left-2">
-          border
+          padding
         </Text>
         <input
           type="number"
           min="0"
-          className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute top-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
-          defaultValue={border.top}
-          onChange={(e) => handleValueChange('border', 'top', e.target.valueAsNumber)}
+          className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute top-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
+          defaultValue={padding.top}
+          onChange={(e) => handleValueChange('padding', 'top', e.target.valueAsNumber)}
         />
         <input
           type="number"
           min="0"
-          className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
-          defaultValue={border.bottom}
-          onChange={(e) => handleValueChange('border', 'bottom', e.target.valueAsNumber)}
+          className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
+          defaultValue={padding.bottom}
+          onChange={(e) => handleValueChange('padding', 'bottom', e.target.valueAsNumber)}
         />
         <input
           type="number"
           min="0"
-          className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 left-0 -translate-y-1/2 text-sm align-middle appearance-none"
-          defaultValue={border.left}
-          onChange={(e) => handleValueChange('border', 'left', e.target.valueAsNumber)}
+          className="w-6 h-6 text-center bg-grey-900 border-none outline-none absolute top-1/2 left-0 -translate-y-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
+          defaultValue={padding.left}
+          onChange={(e) => handleValueChange('padding', 'left', e.target.valueAsNumber)}
         />
         <input
           type="number"
           min="0"
-          className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 right-0 -translate-y-1/2 text-sm align-middle appearance-none"
-          defaultValue={border.right}
-          onChange={(e) => handleValueChange('border', 'right', e.target.valueAsNumber)}
+          className="w-6 h-6 text-center bg-grey-900 border-none outline-none absolute top-1/2 right-0 -translate-y-1/2 text-sm align-middle appearance-none focus:bg-grey-100 transition-colors duration-200"
+          defaultValue={padding.right}
+          onChange={(e) => handleValueChange('padding', 'right', e.target.valueAsNumber)}
         />
 
-        <div className="w-full bg-grey-700 border border-grey-400 rounded p-6 relative">
-          {/* Padding */}
-          <Text type="medium" className="absolute top-1 left-2">
-            padding
-          </Text>
-          <input
-            type="number"
-            min="0"
-            className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute top-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
-            defaultValue={padding.top}
-            onChange={(e) => handleValueChange('padding', 'top', e.target.valueAsNumber)}
-          />
-          <input
-            type="number"
-            min="0"
-            className="h-6 w-9 text-center bg-grey-900 border-none outline-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm align-middle appearance-none"
-            defaultValue={padding.bottom}
-            onChange={(e) => handleValueChange('padding', 'bottom', e.target.valueAsNumber)}
-          />
-          <input
-            type="number"
-            min="0"
-            className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 left-0 -translate-y-1/2 text-sm align-middle appearance-none"
-            defaultValue={padding.left}
-            onChange={(e) => handleValueChange('padding', 'left', e.target.valueAsNumber)}
-          />
-          <input
-            type="number"
-            min="0"
-            className="w-6 h-3 text-center bg-grey-900 border-none outline-none absolute top-1/2 right-0 -translate-y-1/2 text-sm align-middle appearance-none"
-            defaultValue={padding.right}
-            onChange={(e) => handleValueChange('padding', 'right', e.target.valueAsNumber)}
-          />
-
-          <div className="w-full bg-grey-600 border border-grey-400 rounded p-6"></div>
-        </div>
+        <div className="w-full bg-grey-600 border border-grey-400 rounded p-6"></div>
       </div>
     </div>
   );
