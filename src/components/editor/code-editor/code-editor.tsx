f6623fd15c7aa089ff66751a76154e1131813ce8
Wiliam Traoré <hi@wil-liam.com>
2023-07-24 21:26:26 +0200

    fix(code): code editor height

--- src/components/editor/code-editor/code-editor.tsx
+++ src/components/editor/code-editor/code-editor.tsx
@@ -2,12 +2,20 @@ import { Icon } from '../../icon/icon';
 import { Code } from '../code/code';
 import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels';
 
-export function CodeEditor() {
+interface CodeEditorProps {
+  html?: string;
+  css?: string;
+  js?: string;
+}
+
+export function CodeEditor(props: CodeEditorProps) {
+  const { html, css, js } = props;
+
   return (
     <PanelGroup direction="horizontal" className="bg-grey-900 border-t border-grey-400 flex w-full">
       <Panel className="p-4 border-r border-grey-400 relative" collapsible order={1}>
         <p className="mb-3">HTML</p>
-        <Code language="html" className="h-64" />
+        <Code language="html" editorClassName="h-64" initialValue={html} />
       </Panel>
       <PanelResizeHandle className="relative z-10">
         <div className="w-8 h-8 rounded-full border border-grey-400 absolute top-1/2 -right-4 bg-grey-900 flex items-center justify-center">
@@ -16,7 +24,7 @@ export function CodeEditor() {
       </PanelResizeHandle>
       <Panel className="p-4 border-r border-grey-400 relative" collapsible order={1}>
         <p className="mb-3">CSS</p>
-        <Code language="css" className="h-64" />
+        <Code language="css" editorClassName="h-64" initialValue={css} />
       </Panel>
       <PanelResizeHandle className="relative z-10">
         <div className="w-8 h-8 rounded-full border border-grey-400 absolute top-1/2 -right-4 bg-grey-900 flex items-center justify-center">
@@ -25,7 +33,7 @@ export function CodeEditor() {
       </PanelResizeHandle>
       <Panel className="p-4 relative" collapsible order={1}>
         <p className="mb-3">JS</p>
-        <Code language="javascript" className="h-64" />
+        <Code language="javascript" editorClassName="h-64" initialValue={js} />
       </Panel>
     </PanelGroup>
   );
