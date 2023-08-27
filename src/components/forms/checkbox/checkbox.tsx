1c4727dddc5168f2f3db13fcca9b83d6ff6f63d6
Wiliam Traoré <hi@wil-liam.com>
2023-08-27 17:01:13 +0200

    feat(checkbox): add error message

--- src/components/forms/checkbox/checkbox.tsx
+++ src/components/forms/checkbox/checkbox.tsx
@@ -1,4 +1,5 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Text } from '../../text/text';
 
 interface CheckboxProps {
   label: string | React.ReactNode;
@@ -6,22 +7,30 @@ interface CheckboxProps {
   checked?: boolean;
   onChange?: (checked: boolean | 'indeterminate') => void;
   containerClassName?: string;
+  errorMessage?: string;
 }
 
 export function Checkbox(props: CheckboxProps) {
-  const { label, value, checked, onChange, containerClassName } = props;
+  const { label, value, checked, onChange, containerClassName, errorMessage } = props;
 
   return (
-    <div className={`flex items-center gap-3 ${containerClassName}`}>
-      <CheckboxPrimitive.Root
-        defaultChecked={checked}
-        id={value}
-        value={value}
-        onCheckedChange={(checked) => onChange && onChange(checked)}
-        className="w-5 h-5 bg-grey-700 border border-grey-300 rounded data-[state=checked]:bg-primary-500 transition-colors duration-200">
-        <CheckboxPrimitive.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-2 after:h-2 after:rounded-sm after:bg-white" />
-      </CheckboxPrimitive.Root>
-      <label htmlFor={value}>{label}</label>
+    <div className={containerClassName}>
+      <div className={`flex items-center gap-3`}>
+        <CheckboxPrimitive.Root
+          defaultChecked={checked}
+          id={value}
+          value={value}
+          onCheckedChange={(checked) => onChange && onChange(checked)}
+          className="w-5 h-5 bg-grey-700 border border-grey-300 rounded data-[state=checked]:bg-primary-500 transition-colors duration-200">
+          <CheckboxPrimitive.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-2 after:h-2 after:rounded-sm after:bg-white" />
+        </CheckboxPrimitive.Root>
+        <label htmlFor={value}>{label}</label>
+      </div>
+      {errorMessage && (
+        <Text type="medium" className="text-error-500">
+          {errorMessage}
+        </Text>
+      )}
     </div>
   );
 }
