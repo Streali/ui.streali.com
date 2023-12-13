de979e4685e0337dfb0f0b837acd6a1fa86fec8b
Wiliam Traoré <hi@wil-liam.com>
2023-12-13 15:05:49 +0100

    feat(tooltip): add content classname

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -4,10 +4,11 @@ interface TooltipProps {
   children: React.ReactNode;
   content: string;
   delayDuration?: number;
+  contentClassName?: string;
 }
 
 export function Tooltip(props: TooltipProps) {
-  const { children, content, delayDuration = 100 } = props;
+  const { children, content, delayDuration = 100, contentClassName = '' } = props;
 
   return (
     <TooltipPrimitive.Provider>
@@ -15,7 +16,7 @@ export function Tooltip(props: TooltipProps) {
         <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
-            className="bg-black/80 backdrop-blur px-2 py-1 rounded"
+            className={`bg-black/80 backdrop-blur px-2 py-1 rounded ${contentClassName}`}
             sideOffset={8}>
             <TooltipPrimitive.Arrow />
             <p className="text-sm">{content}</p>
