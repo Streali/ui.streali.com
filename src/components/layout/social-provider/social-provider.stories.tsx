a5a008a71a28b73fbd27e328c49064b71dcd943c
Wiliam Traoré <hi@wil-liam.com>
2024-01-04 16:24:13 +0100

    feat(social-provider): change design

--- src/components/layout/social-provider/social-provider.stories.tsx
+++ src/components/layout/social-provider/social-provider.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { SocialProvider } from './social-provider';
+import { Input } from '../../forms/input/input';
 
 const meta: Meta<typeof SocialProvider> = {
   title: 'Layout/Social Provider',
@@ -11,10 +12,14 @@ type Story = StoryObj<typeof SocialProvider>;
 
 export const Default: Story = {
   args: {
-    icon: 'twitch-fill',
-    provider: 'twitch',
-    isConnected: false,
-    isConnectedContent: '@willtraore',
-    connectWithInput: false,
+    icon: 'spotify-fill',
+    name: 'spotify',
+    username: 'willtraore',
+    children: (
+      <>
+        <Input label="Client ID" disabled containerClassName="mb-3" />
+        <Input label="Client secret" disabled />
+      </>
+    ),
   },
 };
--- src/components/layout/social-provider/social-provider.tsx
+++ src/components/layout/social-provider/social-provider.tsx
@@ -10,76 +10,30 @@ interface SocialProviderProps {
   iconWidth?: number;
   iconHeight?: number;
   iconClassName?: string;
-  provider: 'twitch' | 'kick' | 'spotify';
-  isConnected?: boolean;
-  isConnectedContent?: string;
-  onConnectClick?: (value?: string) => void;
-  connectWithInput?: boolean;
-  inputPlaceholder?: string;
+  name: string;
+  children?: React.ReactNode;
+  username?: string;
 }
 
 export function SocialProvider(props: SocialProviderProps) {
-  const {
-    icon,
-    iconSVG,
-    iconClassName,
-    iconHeight,
-    iconWidth,
-    provider,
-    isConnected,
-    isConnectedContent,
-    onConnectClick,
-    connectWithInput,
-    inputPlaceholder = 'Your username',
-  } = props;
-
-  const [inputValue, setInputValue] = useState('');
+  const { icon, iconSVG, iconClassName, iconHeight, iconWidth, children, name, username } = props;
 
   return (
-    <div className="w-full p-6 rounded-lg bg-grey-700 border border-grey-400">
-      <div className="flex justify-between md:items-center flex-col md:!flex-row gap-3">
-        <div>
-          <div className="flex gap-3 items-center h-9">
-            <Icon
-              name={icon}
-              svg={iconSVG}
-              className={`text-2xl text-social-${provider} ${iconClassName}`}
-              width={iconWidth}
-              height={iconHeight}
-            />
-            <span className={`capitalize text-xl text-social-${provider}`}>{provider}</span>
-          </div>
-          <div className="flex gap-1 items-center">
-            <Icon
-              name={isConnected ? 'check-fill' : 'close-line'}
-              className={`${isConnected ? 'text-success-500' : 'text-error-500'}`}
-            />
-            <Text type="medium" className="block -mt-1">
-              {isConnected ? 'Connected' : 'Not connected'}
-            </Text>
-          </div>
+    <div className="w-full p-6 rounded-lg bg-grey-700 border border-grey-400 flex flex-col">
+      <div className="flex gap-3 items-center h-9">
+        <Icon
+          name={icon}
+          svg={iconSVG}
+          className={`text-2xl text-social-${name} ${iconClassName}`}
+          width={iconWidth}
+          height={iconHeight}
+        />
+        <div className="flex flex-col">
+          <span className="text-grey-100">{username}</span>
+          <span className={`capitalize text-xl font-medium -mt-1 text-social-${name}`}>{name}</span>
         </div>
-        {isConnected ? (
-          <div className="flex gap-1 items-center">
-            <div className="inline-flex h-10 px-3 bg-white text-black rounded items-center">
-              {isConnectedContent}
-            </div>
-          </div>
-        ) : (
-          <div className="flex gap-1">
-            {connectWithInput && (
-              <Input
-                placeholder={inputPlaceholder}
-                onChange={(e) => setInputValue(e.target.value)}
-                containerClassName="flex-1 md:flex-0"
-              />
-            )}
-            <Button color="stroke" onClick={() => onConnectClick && onConnectClick(inputValue)}>
-              Connect
-            </Button>
-          </div>
-        )}
       </div>
+      {children && <div className="border-t border-grey-400 mt-6 pt-6">{children}</div>}
     </div>
   );
 }
